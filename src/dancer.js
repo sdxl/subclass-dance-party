ab9968ef1ee588d4ee5d8afa44add90e47a46789
MKS-students <mks.students@hackreactor.com>
2015-08-04 17:22:17 -0500

    Modified init. 'add blinky dancer' button works

--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,5 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
+var Dancer = function(top, left, timeBetweenSteps){
 
   // var dancer = {};
 
@@ -13,13 +13,15 @@ var makeDancer = function(top, left, timeBetweenSteps){
 };
 
 
-  makeDancer.prototype.step = function(){
+  Dancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
+    //look at value of this
+    //console.log to see if function is running
     setTimeout(this.step.bind(this), this.timeBetweenSteps);
   };
 
-  makeDancer.prototype.setPosition = function(top, left){
+  Dancer.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
--- src/init.js
+++ src/init.js
@@ -16,13 +16,14 @@ $(document).ready(function(){
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
+    console.log(typeof dancerMakerFunctionName)
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    console.log(typeof dancerMakerFunction)
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
