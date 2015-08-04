383b6efb6198c38719dc45b9c7357fe8cf8d69e2
MKS-students <mks.students@hackreactor.com>
2015-08-04 12:15:55 -0500

    Refactored

--- src/dancer.js
+++ src/dancer.js
@@ -5,24 +5,27 @@ var makeDancer = function(top, left, timeBetweenSteps){
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.top = top;
+  this.left = left;
   this.step();
-  this.setPosition(top, left);
+  this.setPosition(this.top, this.left);
 };
 
 
   makeDancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(this.step.bind(this), timeBetweenSteps);
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
   };
 
-  makeDancer.setPosition = function(top, left){
+  makeDancer.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
     var styleSettings = {
-      top: top,
-      left: left
+      top: this.top,
+      left: this.left
     };
     this.$node.css(styleSettings);
   };
